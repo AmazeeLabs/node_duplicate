ff80d959ac5cef91607396e00526055d5c1400fb
Alexander Tkachev <alex.leksat@gmail.com>
2018-04-13 09:57:33 +0300

    UH-413 Deal with translations.

--- src/Plugin/Action/DuplicateNodeAction.php
+++ src/Plugin/Action/DuplicateNodeAction.php
@@ -5,6 +5,7 @@ namespace Drupal\node_duplicate\Plugin\Action;
 use Drupal\Core\Action\ActionBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\TypedData\TranslatableInterface;
+use Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList;
 use Drupal\filter\Render\FilteredMarkup;
 
 /**
@@ -19,7 +20,29 @@ use Drupal\filter\Render\FilteredMarkup;
 class DuplicateNodeAction extends ActionBase {
 
   /**
-   * Duplicates a node.
+   * @inheritdoc
+   *
+   * The admin content view displays a row for each node translation. When
+   * several translations of the same node is selected, they all will be passed
+   * to the action. This can be quite confusing for users. Mostly, they expect
+   * to get all translations cloned.
+   *
+   * So we don't pass all translations to execute(), we pass only one per
+   * entity. And execute() iterates over all existing entity translations.
+   */
+  public function executeMultiple(array $entities) {
+    $processed_ids = [];
+    foreach ($entities as $entity) {
+      if (!in_array($entity->id(), $processed_ids, TRUE)) {
+        $this->execute($entity);
+        $processed_ids[] = $entity->id();
+      }
+    }
+  }
+
+
+  /**
+   * Duplicates a node and all its translations.
    *
    * @param \Drupal\node\NodeInterface $entity
    *   Node to duplicate.
@@ -30,20 +53,20 @@ class DuplicateNodeAction extends ActionBase {
   public function execute($entity = NULL) {
     $duplicated_entity = $entity->createDuplicate();
 
-    // Add the "Clone of " prefix to the entity label.
     if ($duplicated_entity instanceof TranslatableInterface) {
       if ($label_key = $duplicated_entity->getEntityType()->getKey('label')) {
         foreach ($duplicated_entity->getTranslationLanguages() as $language) {
           $langcode = $language->getId();
           $duplicated_entity = $duplicated_entity->getTranslation($langcode);
 
+          // Add the "Clone of " prefix to the entity label.
           $new_label = $this->t('Clone of @label', [
             '@label' => FilteredMarkup::create($duplicated_entity->label()),
           ], [
             'langcode' => $langcode,
           ]);
           $duplicated_entity->set($label_key, $new_label);
-
+          // And to the admin title if it exists.
           $admin_title_key = 'field_admin_title';
           if ($duplicated_entity->hasField($admin_title_key)) {
             $current_title = $duplicated_entity->get($admin_title_key)->value;
@@ -56,11 +79,12 @@ class DuplicateNodeAction extends ActionBase {
               $duplicated_entity->set($admin_title_key, $new_admin_title);
             }
           }
+
+          // Clone paragraphs.
+          $this->cloneParagraphs($duplicated_entity);
         }
       }
     }
-
-    $this->cloneEntities($duplicated_entity);
 
     $duplicated_entity->setPublished(FALSE);
     $duplicated_entity->setChangedTime(time());
@@ -69,17 +93,16 @@ class DuplicateNodeAction extends ActionBase {
   }
 
   /**
-   * Clone all entities.
+   * Recursively clones all paragraphs.
    *
-   * @param \Drupal\node\NodeInterface $entity
-   *   Node to duplicate.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
    */
-  protected function cloneEntities($entity) {
+  protected function cloneParagraphs($entity) {
     foreach ($entity as $field) {
-      if ($field instanceof \Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList) {
+      if ($field instanceof EntityReferenceRevisionsFieldItemList) {
         foreach ($field as $field_item) {
           $field_item->entity = $field_item->entity->createDuplicate();
-          $this->cloneEntities($field_item->entity);
+          $this->cloneParagraphs($field_item->entity);
         }
       }
     }
