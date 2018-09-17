19a90e5636393d7d18c7e40228ab13c1ca7349c0
Alexander Tkachev <alex.leksat@gmail.com>
2018-09-17 14:10:40 +0300

    UH-824 Introduce the Clone Referenced setting.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# node_duplicate
+# Node Duplicate
+
 Exposes an action for duplicating nodes.
+
+## Settings
+
+### Clone Referenced
+
+To clone entities of certain types/bundles referenced from the host entity:
+```
+drush -y cset node_duplicate.config clone_referenced.node.teaser 1
+```
+
+This setting is only applied to the "root" entity - the one for which user ran the Duplicate action. For all child entities this setting is ignored.
--- src/Plugin/Action/DuplicateNodeAction.php
+++ src/Plugin/Action/DuplicateNodeAction.php
@@ -3,10 +3,13 @@
 namespace Drupal\node_duplicate\Plugin\Action;
 
 use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\TypedData\TranslatableInterface;
 use Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList;
 use Drupal\filter\Render\FilteredMarkup;
+use Drupal\node\NodeInterface;
 
 /**
  * Duplicates a node.
@@ -18,6 +21,25 @@ use Drupal\filter\Render\FilteredMarkup;
  * )
  */
 class DuplicateNodeAction extends ActionBase {
+
+  /**
+   * Clones of referenced entities.
+   *
+   * Reason: each referenced entity (that has to be cloned) needs to have
+   * exactly one clone.
+   *
+   * Structure:
+   * [
+   *   {entity-type-id} => [
+   *     {original-entity-id} => {entity-clone},
+   *     ...
+   *   ],
+   *   ...
+   * ]
+   *
+   * @var array
+   */
+  protected $clonedChildEntities = [];
 
   /**
    * @inheritdoc
@@ -40,17 +62,36 @@ class DuplicateNodeAction extends ActionBase {
     }
   }
 
+  /**
+   * Duplicates a node and all its translations.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   Node to duplicate.
+   *
+   * @return \Drupal\Core\Entity\ContentEntityInterface
+   *   Duplicated node.
+   */
+  public function execute($entity = NULL) {
+    return $this->cloneEntity($entity, TRUE);
+  }
+
 
   /**
    * Duplicates a node and all its translations.
    *
-   * @param \Drupal\node\NodeInterface $entity
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
    *   Node to duplicate.
+   * @param bool $root_call
+   *   Indicates that the method was called from the execute method.
    *
-   * @return \Drupal\node\NodeInterface
+   * @return \Drupal\Core\Entity\ContentEntityInterface
    *   Duplicated node.
    */
-  public function execute($entity = NULL) {
+  protected function cloneEntity($entity, $root_call) {
+    if ($root_call) {
+      $this->clonedChildEntities = [];
+    }
+
     $duplicated_entity = $entity->createDuplicate();
 
     if ($duplicated_entity instanceof TranslatableInterface) {
@@ -66,6 +107,7 @@ class DuplicateNodeAction extends ActionBase {
             'langcode' => $langcode,
           ]);
           $duplicated_entity->set($label_key, $new_label);
+
           // And to the admin title if it exists.
           $admin_title_key = 'field_admin_title';
           if ($duplicated_entity->hasField($admin_title_key)) {
@@ -79,15 +121,25 @@ class DuplicateNodeAction extends ActionBase {
               $duplicated_entity->set($admin_title_key, $new_admin_title);
             }
           }
-
-          // Clone paragraphs.
-          $this->cloneParagraphs($duplicated_entity);
         }
       }
     }
 
-    $duplicated_entity->setPublished(FALSE);
-    $duplicated_entity->setChangedTime(time());
+    // Clone paragraphs and referenced entities (if required).
+    foreach ($duplicated_entity->getTranslationLanguages() as $language) {
+      $langcode = $language->getId();
+      $duplicated_entity = $duplicated_entity->getTranslation($langcode);
+      $this->cloneParagraphs($duplicated_entity);
+      if ($root_call) {
+        $this->cloneReferencedEntities($duplicated_entity);
+      }
+    }
+
+    if ($duplicated_entity instanceof NodeInterface) {
+      $duplicated_entity->setPublished(FALSE);
+      $duplicated_entity->setChangedTime(time());
+    }
+
     if ($entity->hasField('path')) {
       $duplicated_entity->path = [
         'alias' => null,
@@ -112,6 +164,52 @@ class DuplicateNodeAction extends ActionBase {
         foreach ($field as $field_item) {
           $field_item->entity = $field_item->entity->createDuplicate();
           $this->cloneParagraphs($field_item->entity);
+        }
+      }
+    }
+  }
+
+  /**
+   * Clones referenced entities if it's required by the config.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function cloneReferencedEntities($entity) {
+    $enabled = \Drupal::config('node_duplicate.config')->get('clone_referenced');
+    if (empty($enabled)) {
+      return;
+    }
+
+    foreach ($entity as $field) {
+      if ($field instanceof EntityReferenceRevisionsFieldItemList) {
+        foreach ($field as $field_item) {
+          $this->cloneReferencedEntities($field_item->entity);
+        }
+      }
+      elseif ($field instanceof EntityReferenceFieldItemList) {
+        foreach ($field as $field_item) {
+          $child_entity = $field_item->entity;
+          if ($child_entity instanceof ContentEntityInterface) {
+            $type = $child_entity->getEntityTypeId();
+            $bundle = $child_entity->bundle();
+            if (!empty($enabled[$type][$bundle])) {
+              $id = $child_entity->id();
+              if (!isset($this->clonedChildEntities[$type][$id])) {
+                $clone = $this->cloneEntity($child_entity, FALSE);
+                $this->clonedChildEntities[$type][$id] = $clone;
+                // It can be possible that entity translations refer to the same
+                // child entity. In this case, when we process the first
+                // translation, we override the child entity with a clone. Then,
+                // when we process the second translation, it already refers to
+                // the clone, so we can accidentally clone a clone.
+                // To avoid this, also save the clone ID in clonedChildEntities.
+                $this->clonedChildEntities[$type][$clone->id()] = $clone;
+              }
+              $field_item->entity = $this->clonedChildEntities[$type][$id];
+            }
+          }
         }
       }
     }
